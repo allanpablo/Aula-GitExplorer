9da3ac62af57ac6304bd9b9292e96892d54fad57
ApK <apk@Mac-Pro-de-ApK.local>
2021-05-04 00:13:22 -0300

    fast Refresh

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-import { Counter } from './components/Counter';
-import { RepositoryList } from './components/RepositoryList';
-import './styles/global.scss';
+import { RepositoryList } from "./components/RepositoryList";
+import "./styles/global.scss";
 
 export function App() {
-//throw new Error('Eita Mayra, o Microfone Caiu!!!')
+  //throw new Error('Eita Mayra, o Microfone Caiu!!!')
 
-  return ( 
-  <>
-  <RepositoryList />
-  <Counter />
-  </>
+  return (
+    <>
+      <RepositoryList />
+    </>
   );
 }
